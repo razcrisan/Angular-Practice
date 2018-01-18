dcbf7451d3af46c83e07dab07da182e893f3b929
Razvan Crisan <rcrisan13@gmail.com>
2018-01-18 13:54:27 -0500

    text boxes have empty box under them

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-success-alert',
   template: `<p> You have succeeded!!! Great Job. <p>`,
-  styles: ['p {padding:20px; background-color:green;']
+  styles: ['p {padding:20px; background-color:palegreen; border: 1px solid green;']
 })
 export class SuccessAlertComponent implements OnInit {
 
