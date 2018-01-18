ffa02cbd5aff5463be7326a333f77ff6ca2097b9
Razvan Crisan <rcrisan13@gmail.com>
2018-01-18 13:39:33 -0500

    added WarningAlert and SuccessAlert

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component, } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: `
+    <p> This is a warning, You are in danger! <p>`,
+  styleUrls: ['./warning-alert.component.css']
+})
+export class WarningAlertComponent {
+
+  constructor() { }
+
+
+}
