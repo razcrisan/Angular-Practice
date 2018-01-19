8eb22c1cbd836a18d3130e2a765e28ba7faa6751
Razvan Crisan <rcrisan13@gmail.com>
2018-01-18 21:43:43 -0500

    added warning and success buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  title: 'app';
+  clickedSuccess = true;
+  clickedWarning = true;
+  public showSuccessMessage() {
+    if (this.clickedSuccess) {
+      this.clickedSuccess = false;
+    }
+    else {
+      this.clickedSuccess = true;
+    }
+  }
+  public showWarningMessage() {
+    if (this.clickedWarning) {
+      this.clickedWarning = false;
+    }
+    else {
+      this.clickedWarning = true;
+    }
+  }
 }
